4ff7d1588f49587f4f7b09a3bda42d6a20f080fb
Anna Tong <tonga@ripple.com>
2014-10-16 18:02:12 -0700

    attempt 2

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express=require('express')
 var app=express()
 var server=http.createServer(app)
 var bp=require('body-parser')
+var request=require('request')
+var response=require('response')
 app.use(bp.json())
 server.listen(3000)
 
@@ -14,4 +16,17 @@ app.post('/submitname',function(req,res){
 	console.log(req.headers)
 	console.log(req.method,req.url)
 	console.log("here is the body",req.body)
+	request("https://id.ripple.com/v1/user/"+req.body.name, {json:true}, function(error,response,body){
+	var obj={}
+	if (body.exists==true){
+		obj.exists=true
+		obj.username=body.username
+		obj.address=body.address
+	}
+	else {
+		obj.exists=false
+	}
+response.json(obj).pipe(res)
 })
+})
+
