c938e2780913ebee54c26ad4816dd05c2d4eb050
J.M <jm@ripple.com>
2014-10-23 21:12:05 +0200

    [TASK] Pretty-format all the code
    Like adding semicolons and using a proper DOCTYPE declaration.

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-var http=require('http')
-var express=require('express')
-var app=express()
-var server=http.createServer(app)
-var bp=require('body-parser')
-var request=require('request')
-var response=require('response')
-app.use(bp.json())
-server.listen(3000)
+var http     = require('http'),
+    express  = require('express'),
+    app      = express(),
+    server   = http.createServer(app),
+    bp       = require('body-parser'),
+    request  = require('request'),
+    response = require('response');
+app.use(bp.json());
+server.listen(3000);
 
-//in browser, go to http://127.0.0.1:3000/
+// in browser, go to http://127.0.0.1:3000/
 
-var ecstatic=require('ecstatic')
-app.use(ecstatic('./web'))
-app.post('/submitname',function(req,res){
-	request("https://id.ripple.com/v1/user/"+req.body.name, {json:true}, function(error,resp,body){
-        var obj={}
-        if (body.exists==true){
-            obj.exists=true
-            obj.username="~"+body.username
-            obj.address=body.address
-        }
-        else {
-            obj.exists=false
-        }
-        response.json(obj).status(200).pipe(res)
-    })
-})
-
+var ecstatic = require('ecstatic');
+app.use(ecstatic('./web'));
+app.post('/submitname', function(req,res) {
+    request(
+        "https://id.ripple.com/v1/user/" + req.body.name,
+        { json:true },
+            function(error, resp, body) {
+            var obj = {};
+            if (body.exists === true) {
+                obj.exists   = true;
+                obj.username = "~" + body.username;
+                obj.address  = body.address;
+            }
+            else {
+                obj.exists = false;
+            }
+            response.json(obj).status(200).pipe(res);
+        });
+});
